7a7d89bdc26e9ee7816688ccfce377b4065baa3a
Rusty Jones <rusty.jones1995@gmail.com>
2024-05-27 00:44:09 -0500

    Added bar chart component and logic to populate it with data

--- student-activity-ui/src/app/app.component.ts
+++ student-activity-ui/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StudentTableComponent } from './student-table/student-table.component';
+import { StudentBarChartComponent } from './student-bar-chart/student-bar-chart.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, StudentTableComponent],
+  imports: [RouterOutlet, StudentTableComponent, StudentBarChartComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ student-activity-ui/src/app/student-bar-chart/student-bar-chart.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+import { ShareDataService } from '../share-data.service';
+import Chart from 'chart.js/auto';
+
+@Component({
+  selector: 'student-bar-chart',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-bar-chart.component.html',
+  styleUrl: './student-bar-chart.component.css'
+})
+export class StudentBarChartComponent {
+  public studentChart: any;
+  private students: any[] = [];
+  private activityMap: Map<string, number> = new Map<string, number>();
+  private subscriptionCallCount: number = 1;
+
+  constructor(private shareDataService: ShareDataService) { }
+
+  ngOnInit() {
+    this.shareDataService.dataSource.subscribe((data) => {
+      if (this.subscriptionCallCount > 1) {
+        console.log('hello from inside if statement');
+        this.students = data;
+        this.createLabelsAndDataSets();
+        this.createStudentChart();
+      }
+      this.subscriptionCallCount = this.subscriptionCallCount + 1;
+      console.log('hello subscription call count: ' + this.subscriptionCallCount);
+    });
+  }
+
+  createLabelsAndDataSets() {
+    this.students.forEach(student => {
+      if (!this.activityMap.has(student.activity)) {
+        console.log('Hello from init to 1');
+        this.activityMap.set(student.activity, 1);
+      } else {
+        var count = this.activityMap.get(student.activity);
+        if (count != undefined && count != null) {
+          count = count + 1;
+          this.activityMap.set(student.activity, count);
+          console.log('New activity map value: ' + this.activityMap.get(student.activity));
+        }
+      }
+    });
+    console.log(Array.from(this.activityMap.keys()));
+    console.log(Array.from(this.activityMap.values()));
+    this.createStudentChart();
+  }
+
+  createStudentChart() {
+    this.studentChart = new Chart("StudentChart", {
+      type: 'bar',
+
+      data: {
+        labels: Array.from(this.activityMap.keys()), 
+	       datasets: [
+          {
+            label: "Student Activity",
+            data: Array.from(this.activityMap.values()),
+            backgroundColor: 'blue'
+          }
+        ]
+      },
+      options: {
+        aspectRatio:2.5,
+        responsive: false
+      }
+    });
+  }
+}
